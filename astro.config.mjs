ba8649048066621a444a084305f7e595a404562b
Lee-projecthelios <eve.projecthelios@gmail.com>
2023-03-18 20:21:07 +0000

    Added Legal notice
    Legal noticed added, some slight UI tweeks here and there also.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import image from "@astrojs/image";
+import prefetch from "@astrojs/prefetch";
+
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [image(), prefetch(), tailwind(), sitemap()]
+});
